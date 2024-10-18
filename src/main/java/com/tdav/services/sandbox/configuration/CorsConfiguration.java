d884fe951108cac2cbf63efe9c368ae8875f0e18
Tom Rusch <tommy_rusch@aol.com>
2024-10-18 16:17:34 -0400

    added CORS config

---
+++ src/main/java/com/tdav/services/sandbox/configuration/CorsConfiguration.java
@@ -0,0 +1,15 @@
+package com.tdav.services.sandbox.configuration;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfiguration implements WebMvcConfigurer {
+  
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+      .allowedOrigins("localhost:3000", "http://tdavikman.com")
+      .allowedMethods("GET", "POST", "PUT", "DELETE");
+  }
+
+}
--- src/test/java/com/tdav/services/sandbox/SandboxApplicationTests.java
+++ src/test/java/com/tdav/services/sandbox/SandboxApplicationTests.java
@@ -1,6 +1,5 @@
 package com.tdav.services.sandbox;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
