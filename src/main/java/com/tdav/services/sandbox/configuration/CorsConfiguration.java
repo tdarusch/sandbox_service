fd16fc10f395036ed570d46c4bce1fc3a3f63cd3
Tom Rusch <tommy_rusch@aol.com>
2024-11-16 00:44:27 -0500

    update localhost port

--- src/main/java/com/tdav/services/sandbox/configuration/CorsConfiguration.java
+++ src/main/java/com/tdav/services/sandbox/configuration/CorsConfiguration.java
@@ -8,7 +8,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-      .allowedOrigins("http://localhost:3000", "http://tdavikman.com")
+      .allowedOrigins("http://localhost:5173", "https://tdavikman.com")
       .allowedMethods("GET", "POST", "PUT", "DELETE");
   }
 
