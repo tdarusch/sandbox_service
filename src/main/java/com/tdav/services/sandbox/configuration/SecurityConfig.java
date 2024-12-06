9dd76f1073237bda5311118df2a9c94e45b73cd7
Tom Rusch <tommy_rusch@aol.com>
2024-12-06 02:28:28 -0500

    cors config

--- src/main/java/com/tdav/services/sandbox/configuration/SecurityConfig.java
+++ src/main/java/com/tdav/services/sandbox/configuration/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://tdavikman.com"));
+    configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://tdavikman.com", "https://tdavsandbox.netlify.app"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
